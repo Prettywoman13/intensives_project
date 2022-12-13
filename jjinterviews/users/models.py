a8a9916f45f42bbea51312d815f34a62b3f5701c
Alexander Permyakov <nathan920@yandex.ru>
2022-12-13 22:17:43 +0300

    isort fix

--- jjinterviews/users/models.py
+++ jjinterviews/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.core.mail import send_mail
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
 from users.managers import UserManager
 
 
