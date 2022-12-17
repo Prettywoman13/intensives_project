f858003232e25b3cedf81e4a34f0d41c4683b3e3
Alexander Permyakov <nathan920@yandex.ru>
2022-12-17 19:20:25 +0300

    small style fix + register button

--- jjinterviews/users/forms.py
+++ jjinterviews/users/forms.py
@@ -14,7 +14,7 @@ class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         self.fields["password1"].label = "Пароль"
-        self.fields["password2"].label = "Пароль"
+        self.fields["password2"].label = "Повторите пароль"
         for visible in self.visible_fields():
             visible.field.widget.attrs["class"] = "form-control"
             visible.field.widget.attrs["placeholder"] = visible.field.label
