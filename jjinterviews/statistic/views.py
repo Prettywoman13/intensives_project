6abf6ca2f4bb18ccbba9f7418c4e399ffc98d846
Alexander Permyakov <nathan920@yandex.ru>
2022-12-20 18:41:16 +0300

    add login mixin

--- jjinterviews/statistic/views.py
+++ jjinterviews/statistic/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from interviews.models import InterviewStatistic
 
 
-class StatisticMainPage(ListView):
+class StatisticMainPage(LoginRequiredMixin, ListView):
     model = InterviewStatistic
     template_name = "pages/statistic/main.html"
     context_object_name = "statistics"
