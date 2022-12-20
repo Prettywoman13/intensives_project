8ac3bcfb4096834a402f6240d35ae6284f07e711
irkatsuk <irkatsuk@yandex.ru>
2022-12-20 21:25:19 +0700

    add new question form

---
+++ jjinterviews/questions/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import NewQuestion
+
+app_name = "questions"
+
+urlpatterns = [path("new_question/", NewQuestion.as_view(),
+               name="new_question")]
