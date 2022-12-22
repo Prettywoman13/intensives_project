b073df320d489fb8e09853d2487f034306173ec6
Prettywoman13 <AveSvecha@yandex.ru>
2022-12-22 15:37:19 +0300

    fix code in statistic

--- jjinterviews/interviews/views.py
+++ jjinterviews/interviews/views.py
@@ -1,5 +1,5 @@
-from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, reverse
 from django.views.generic import FormView
